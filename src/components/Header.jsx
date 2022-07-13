a83ebe0c600033592e9044502396d4b67e9f52f1
Dmitry <100623236+General-123@users.noreply.github.com>
2022-07-13 15:28:18 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className="header">
+  <img
+    src='https://www.svgrepo.com/show/327388/logo-react.svg'
+    alt=""
+  ></img>
+</header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className="content">
+  <div>
+    <img
+      src="https://cdnn21.img.ria.ru/images/rsport/111828/79/1118287936_0:380:2963:2047_1920x0_80_0_0_97d83b8792c35e9d3ac7817e1566c8d8.jpg"
+      alt=""
+    ></img>
+  </div>
+
+  <div>ava + discription</div>
+
+  <div>
+    My post
+    <div>New post</div>
+  </div>
+
+  <div>
+    <div>post1</div>
+    <div>post2</div>
+  </div>
+</div>
+}
+
+export default Profile;
